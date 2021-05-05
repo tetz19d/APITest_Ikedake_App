8c73d51127f0b6c425da67486247dfe6ada144e8
kanade <aquacurasta@gmail.com>
2021-05-05 16:31:31 +0900

    不要なimportを削除

--- src/main/java/com/example/demo/restservice/koichanController.java
+++ src/main/java/com/example/demo/restservice/koichanController.java
@@ -4,10 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-import java.util.Objects;
-import java.text.SimpleDateFormat;
-
 @RestController
 public class koichanController {
 
